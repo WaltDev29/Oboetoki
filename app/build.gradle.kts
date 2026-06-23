8cc4a573a2b166c1157f3d68e4cb53fb183b2b8c
WaltDev29 <waltdev29@gmail.com>
2026-06-24 03:05:02 +0900

    Feat: API 경로 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        
+        buildConfigField("String", "BASE_URL", "\"https://nextwave.aikopo.net/oboetoki/api\"")
     }
 
     buildTypes {
@@ -35,6 +37,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
